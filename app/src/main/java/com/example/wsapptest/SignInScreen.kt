1600b48c901baca8fcd1a6de9e37374f496c1b4b
AnGeLZhiL <angelina.zhilyakova.2002@mail.ru>
2022-11-21 10:29:04 +0300

    Экран авторизации

--- app/src/main/java/com/example/wsapptest/SignInScreen.kt
+++ app/src/main/java/com/example/wsapptest/SignInScreen.kt
@@ -1,11 +1,22 @@
 package com.example.wsapptest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.wsapptest.databinding.ActivitySignInScreenBinding
 
 class SignInScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in_screen)
+        binding = ActivitySignInScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun SignUp_onClick(view: View) {
+        startActivity(Intent(this, SignInScreen::class.java))
     }
 }
--- app/src/main/java/com/example/wsapptest/SignUpScreen.kt
+++ app/src/main/java/com/example/wsapptest/SignUpScreen.kt
@@ -1,11 +1,22 @@
 package com.example.wsapptest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.wsapptest.databinding.ActivitySignUpScreenBinding
 
 class SignUpScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up_screen)
+        binding = ActivitySignUpScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun SignIn_onClick(view: View) {
+        startActivity(Intent(this, SignInScreen::class.java))
     }
 }
